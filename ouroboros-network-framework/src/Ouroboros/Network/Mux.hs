07fcf53e704c869cabdc0fcbb99ff1f2cb7eff11
Marcin Szamotulski <profunctor@pm.me>
2020-06-08 16:57:29 +0200

    HotApp / WarmApp and EstablishedApp distinction
    The governor needs to take care about three types of protocols: ones
    that run for warm peer and hot peers.  For this purpose we provide
    a distinction into three disjoint subsets: `WarmApp` warm protocols,
    which run for  warm peers, `HotApp` for hot peers and `EstablishedApp`
    for protocols which must run in for either hot or warm peers
    (established peers in the language for p2p-governor).  Using GADTs we
    make this distinction at type level.  This patch provides convientient
    functions to work with the introduced complexity.

--- ouroboros-network-framework/src/Ouroboros/Network/Mux.hs
+++ ouroboros-network-framework/src/Ouroboros/Network/Mux.hs
@@ -1,12 +1,26 @@
+{-# LANGUAGE BangPatterns        #-}
 {-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveFunctor       #-}
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE FlexibleInstances   #-}
 {-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE NamedFieldPuns      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving  #-}
 
 module Ouroboros.Network.Mux
   ( MuxMode (..)
   , OuroborosApplication (..)
+  , MuxProtocolBundle
+  , AppKind (..)
+  , TokAppKind (..)
+  , WithAppKind (..)
+  , withoutAppKind
+  , Bundle (..)
+  , projectBundle
+  , OuroborosBundle
+  , MuxBundle
   , MiniProtocol (..)
   , MiniProtocolNum (..)
   , MiniProtocolLimits (..)
@@ -14,6 +28,7 @@ module Ouroboros.Network.Mux
   , MuxPeer (..)
   , runMuxPeer
   , toApplication
+  , mkMuxApplicationBundle
   , ouroborosProtocols
   , RunOrStop (..)
   , ScheduledStop
@@ -52,6 +67,7 @@ import           Ouroboros.Network.Driver
 
 
 data RunOrStop = Run | Stop
+  deriving (Eq, Show)
 
 -- |  'ScheduleStop' should depend on `muxMode` (we only need to shedule stop
 -- for intiator side).  This is not done only because this would break tests,
@@ -63,15 +79,120 @@ neverStop :: Applicative (STM m)
           -> ScheduledStop m
 neverStop _ = pure Run
 
-
 -- |  Like 'MuxApplication' but using a 'MuxPeer' rather than a raw
 -- @Channel -> m a@ action.
 --
 newtype OuroborosApplication (mode :: MuxMode) addr bytes m a b =
         OuroborosApplication
-          (ConnectionId addr
-            -> STM m RunOrStop
-            -> [MiniProtocol mode bytes m a b])
+          (ConnectionId addr -> STM m RunOrStop -> [MiniProtocol mode bytes m a b])
+
+
+-- |  There are three kinds of applications: warm, hot and established (ones
+-- that run in for both warm and hot peers).
+--
+data AppKind = HotApp | WarmApp | EstablishedApp
+
+
+-- | Singletons for 'AppKind'
+--
+data TokAppKind (appKind :: AppKind) where
+    TokHotApp         :: TokAppKind HotApp
+    TokWarmApp        :: TokAppKind WarmApp
+    TokEstablishedApp :: TokAppKind EstablishedApp
+
+
+-- | We keep hot and warm application (or their context) distinct.  It's only
+-- needed for a handly 'projectBundle' map.
+--
+data WithAppKind (appKind :: AppKind) a where
+    WithHot         :: !a -> WithAppKind HotApp  a
+    WithWarm        :: !a -> WithAppKind WarmApp a
+    WithEstablished :: !a -> WithAppKind EstablishedApp a
+
+deriving instance Eq a => Eq (WithAppKind appKind a)
+deriving instance Show a => Show (WithAppKind appKind a)
+deriving instance (Functor (WithAppKind appKind))
+
+instance Semigroup a => Semigroup (WithAppKind appKind a) where
+    WithHot a <> WithHot b = WithHot (a <> b)
+    WithWarm a <> WithWarm b = WithWarm (a <> b)
+    WithEstablished a <> WithEstablished b = WithEstablished (a <> b)
+
+instance Monoid a => Monoid (WithAppKind HotApp a) where
+    mempty = WithHot mempty
+
+instance Monoid a => Monoid (WithAppKind WarmApp a) where
+    mempty = WithWarm mempty
+
+instance Monoid a => Monoid (WithAppKind EstablishedApp a) where
+    mempty = WithEstablished mempty
+
+
+withoutAppKind :: WithAppKind appKind a -> a
+withoutAppKind (WithHot a)         = a
+withoutAppKind (WithWarm a)        = a
+withoutAppKind (WithEstablished a) = a
+
+
+-- | A bundle of 'HotApp', 'WarmApp' and 'EstablishedApp'.
+--
+data Bundle a =
+      Bundle {
+          -- | hot mini-protocols
+          --
+          withHot
+            :: !(WithAppKind HotApp a),
+
+          -- | warm mini-protocols
+          --
+          withWarm
+            :: !(WithAppKind WarmApp a),
+
+          -- | established mini-protocols
+          --
+          withEstablished
+            :: !(WithAppKind EstablishedApp a)
+        }
+  deriving (Eq, Show, Functor)
+
+instance Semigroup a => Semigroup (Bundle a) where
+    Bundle hot warm established <> Bundle hot' warm' established' =
+      Bundle (hot <> hot')
+             (warm <> warm')
+             (established <> established')
+
+instance Monoid a => Monoid (Bundle a) where
+    mempty = Bundle mempty mempty mempty
+
+projectBundle :: TokAppKind appKind -> Bundle a -> a
+projectBundle TokHotApp         = withoutAppKind . withHot
+projectBundle TokWarmApp        = withoutAppKind . withWarm
+projectBundle TokEstablishedApp = withoutAppKind . withEstablished
+
+
+instance Applicative Bundle where
+    pure a = Bundle (WithHot a) (WithWarm a) (WithEstablished a)
+    Bundle (WithHot hotFn)
+           (WithWarm warmFn)
+           (WithEstablished establishedFn)
+      <*> Bundle (WithHot hot)
+                 (WithWarm warm)
+                 (WithEstablished established) =
+        Bundle (WithHot $ hotFn hot)
+               (WithWarm $ warmFn warm)
+               (WithEstablished $ establishedFn established)
+
+--
+-- Useful type synonyms
+--
+
+type MuxProtocolBundle (mode :: MuxMode) addr bytes m a b
+       = ConnectionId addr
+      -> STM m RunOrStop
+      -> [MiniProtocol mode bytes m a b]
+
+type OuroborosBundle (mode :: MuxMode) addr bytes m a b =
+    Bundle (MuxProtocolBundle mode addr bytes m a b)
 
 data MiniProtocol (mode :: MuxMode) bytes m a b =
      MiniProtocol {
@@ -79,6 +200,10 @@ data MiniProtocol (mode :: MuxMode) bytes m a b =
        miniProtocolLimits :: !MiniProtocolLimits,
        miniProtocolRun    :: !(RunMiniProtocol mode bytes m a b)
      }
+
+type MuxBundle (mode :: MuxMode) bytes m a b =
+    Bundle [MiniProtocol mode bytes m a b]
+
 
 data RunMiniProtocol (mode :: MuxMode) bytes m a b where
      InitiatorProtocolOnly
@@ -126,6 +251,46 @@ toApplication connectionId scheduleStop (OuroborosApplication ptcls) =
         Mux.miniProtocolRun    = toMuxRunMiniProtocol (miniProtocolRun ptcl)
       }
     | ptcl <- ptcls connectionId scheduleStop ]
+
+
+mkMuxApplicationBundle
+    :: forall mode addr bytes m a b.
+       ConnectionId addr
+    -> Bundle (ScheduledStop m)
+    -> OuroborosBundle mode addr bytes m a b
+    -> MuxBundle       mode      bytes m a b
+mkMuxApplicationBundle connectionId
+                       (Bundle
+                         hotScheduleStop
+                         warmScheduleStop
+                         establishedScheduleStop)
+                       (Bundle
+                           hotApp
+                           warmApp
+                           establishedApp) =
+    Bundle {
+        withHot =
+          mkApplication hotScheduleStop hotApp,
+
+        withWarm =
+          mkApplication warmScheduleStop warmApp,
+
+        withEstablished =
+          mkApplication establishedScheduleStop establishedApp
+    }
+  where
+    mkApplication :: WithAppKind appKind (ScheduledStop m)
+                  -> WithAppKind appKind (MuxProtocolBundle mode addr bytes m a b)
+                  -> WithAppKind appKind [MiniProtocol mode bytes m a b]
+    mkApplication (WithHot scheduleStop) (WithHot app) =
+      WithHot $ app connectionId scheduleStop
+
+    mkApplication (WithWarm scheduleStop) (WithWarm app) =
+      WithWarm $ app connectionId scheduleStop
+
+    mkApplication (WithEstablished scheduleStop) (WithEstablished app) =
+      WithEstablished $ app connectionId scheduleStop
+
 
 toMuxRunMiniProtocol :: forall mode m a b.
                         (MonadCatch m, MonadAsync m)
